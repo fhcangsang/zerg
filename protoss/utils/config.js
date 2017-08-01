368a313842b658df2b09d37f5d08eed3d57c55b6
mingzu <675784774@qq.com>
2017-08-01 13:50:09 +0800

    ss

--- protoss/utils/config.js
+++ protoss/utils/config.js
@@ -1,10 +1,11 @@
-//config.js
-class Config {
-  constructor(){
 
-  }
+class Config{
+    constructor(){
+
+    }
 }
-//Config.restUrl = 'http://test.tp5.com/api/v1/'; //本地测试
-//Config.restUrl = 'http://wxtpwx.duapp.com/index.php/api/v1/'; //百度云服务器
-Config.restUrl = 'https://www.skeblog.top/api/v1/';  //知了云服务器
+
+Config.restUrl = 'https://www.skeblog.top/api/v1/';
+Config.onPay=true;  //是否启用支付
+
 export {Config};
