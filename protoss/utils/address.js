2f20fab71cb7775546ebcb539e7503306a60369c
mingzu <675784774@qq.com>
2017-08-01 14:16:50 +0800

    yes

--- protoss/utils/address.js
+++ protoss/utils/address.js
@@ -14,6 +14,7 @@ class Address extends Base{
         var that=this;
         var param={
             url: 'address',
+            type: 'post',
             sCallback:function(res){
                 if(res) {
                     res.totalDetail = that.setAddressInfo(res);
